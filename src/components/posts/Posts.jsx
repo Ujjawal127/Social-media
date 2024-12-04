a2be5fff655007ddefb04a54b8e8d83215ae243b
Ujjawal127 <ujjwalr781@gmail.com>
2024-12-04 21:14:27 +0530

    updated code

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,33 @@
+import Post from "../post/Post";
+import "./posts.scss";
+
+const Posts = () => {
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "Rohit",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "HI Good Morning to all",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "MOhit",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Hello what's up Guys",
+    },
+  ];
+
+  return <div className="posts">
+    {posts.map(post=>(
+      <Post post={post} key={post.id}/>
+    ))}
+  </div>;
+};
+
+export default Posts;
